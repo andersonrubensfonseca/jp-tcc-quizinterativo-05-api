8d2466b4ff682cc0653d0e8730a18f443d4b8d25
Maria Vitoria <mariavitoriavalory@outlook.com>
2024-08-09 15:44:13 -0300

    add o update e delete, fizemos todo o CRUD

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -8,7 +8,25 @@ async index(req,res) {
         const usuario = await prisma.users.create(req.body)
         res.status(201).json({message:'Usuário criado com sucesso.'})
     }
+    async update(req,res){
+        const updateUsers = await prisma.user.updateMany({
+        where: {
+          email: 'viola@prisma.io',
+        },
+        data: {
+          name: 'Viola the Magnificent',
+        },
+      })
+      res.status(201).json({message:'Usuário atualizado com sucesso.'})
 }
-
-
+    async delete(req,res){
+        const deleteUsers = await prisma.user.deleteMany({
+            where: {
+            email: {
+            contains: 'prisma.io',
+      },
+    },
+  })
+}
+}
 module.exports = UsersController;
